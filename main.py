7fa36fd2239dd87b887d21a9cb08896697ce6bd7
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2026-04-20 16:53:14 +0300

    decompose quickstart guide into get_client and ask functions

--- main.py
+++ main.py
@@ -8,19 +8,28 @@ if os.path.exists(dotenv_path):
 
 
 def main():
+    client = get_client()
+    answer = ask(client, "Как дела?")
+    print(answer)
+
+
+def get_client() -> openai.OpenAI:
     client = openai.OpenAI(
         api_key=os.getenv("YANDEX_API_KEY"),
         project=os.getenv("YANDEX_FOLDER_ID"),
         base_url="https://ai.api.cloud.yandex.net/v1"
     )
+    return client
+
+
+def ask(client: openai.OpenAI, query: str) -> str:
     response = client.responses.create(
         model=f'gpt://{os.getenv("YANDEX_FOLDER_ID")}/{os.getenv("YANDEX_MODEL")}',
-        input="Как дела?",
+        input=query,
         temperature=0.8,
         max_output_tokens=1500
     )
-
-    print(response.output[0].content[0].text)  # ty: ignore[unresolved-attribute, not-subscriptable]
+    return response.output_text
 
 
 if __name__ == "__main__":
