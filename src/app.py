881ed5c341841f64de0add1b40ca540a0c9ca3bc
Evgeny  Uskov <80920825+euskov17@users.noreply.github.com>
2026-07-07 22:43:53 +0300

    Add multi agent setup, support yaml config (#13)
    * initial add coordinator, one-prompt
    
    * move main into app
    
    * remove duplicate agent classes
    
    * make base agent class
    
    * support_yaml_config

--- src/app.py
+++ src/app.py
@@ -1,29 +1,51 @@
+import logging
+import sys
+import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from message_service import MessageService
-from config import Settings
+from config import load_config, AppConfig
 from bot_handlers import create_router
-from context import UserSecretsStore
+from context import UserStore
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    datefmt="%H:%M:%S",
+    stream=sys.stderr,
+)
 
 
-def create_app(settings: Settings) -> tuple[Bot, Dispatcher]:
+def create_app(config: AppConfig) -> tuple[Bot, Dispatcher]:
     bot = Bot(
-        token=settings.bot_token,
+        token=config.bot.bot_token,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
     dp = Dispatcher()
 
-    secrets_store = UserSecretsStore()
-    message_service = MessageService(settings)
+    users_store = UserStore()
+    message_service = MessageService(config)
 
     router = create_router(
-        settings=settings,
         bot=bot,
-        secrets_store=secrets_store,
         message_service=message_service,
+        session_db=config.session_db_config,
+        paths=config.paths,
+        user_store=users_store,
     )
 
     dp.include_router(router)
     return bot, dp
+
+
+async def main() -> None:
+    logging.getLogger(__name__).info("Main started")
+    config: AppConfig = load_config()
+    bot, dp = create_app(config)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
